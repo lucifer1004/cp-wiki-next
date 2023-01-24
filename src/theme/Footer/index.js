b2ddecc9d1143a7075f504fbf2625183c01f68b3
Gabriel Wu <wuzihua@pku.edu.cn>
2023-01-24 16:16:21 +0800

    feat: giscuz

--- src/theme/Footer/index.js
+++ src/theme/Footer/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import Footer from '@theme-original/Footer';
-import Utterances from '@site/src/components/Utterances';
+import Giscus from '@giscus/react';
+import { translate } from '@docusaurus/Translate';
 
 export default function FooterWrapper(props) {
   return (
     <>
-      <Utterances />
+      <Giscus
+        id="comments"
+        repo="lucifer1004/cp-wiki-next"
+        repoId="R_kgDOI0xGOQ"
+        category="Announcements"
+        categoryId="DIC_kwDOI0xGOc4CTxUn"
+        mapping="pathname"
+        // term="Welcome to @giscus/react component!"
+        reactionsEnabled="1"
+        emitMetadata="0"
+        inputPosition="top"
+        theme="preferred_color_scheme"
+        lang={translate({
+          message: "zh-CN",
+          id: "theme.Giscus.lang",
+        })}
+        loading="lazy"
+      />
       <Footer {...props} />
     </>
   );
